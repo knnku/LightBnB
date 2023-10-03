81efa0bb85496edf0a0351e1b8bfb343a3c624e0
Kenniku Tolato <kenniku_you@ymail.com>
2023-10-03 03:33:10 +0000

    Add b/e sql feature for getAllProperties in promises.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,18 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb"
+});
+
+pool.query(`SELECT title FROM properties LIMIT 10;`).then((res) => {
+  // console.log(res);
+});
+
 
 /// Users
 
@@ -8,6 +21,7 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithEmail = function (email) {
   let resolvedUser = null;
   for (const userId in users) {
@@ -59,12 +73,19 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
+const getAllProperties = (options, limit = 10) => {
   const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool
+    .query(
+      `SELECT * FROM properties LIMIT $1 `,
+      [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
